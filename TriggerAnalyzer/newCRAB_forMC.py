4517e53db9fbef7e2458a94e06fb6e450e9b9805
Abhisek Datta <abdatta@cern.ch>
2016-11-22 00:05:37 -0500

    Update newCRAB_forMC.py

--- TriggerAnalyzer/newCRAB_forMC.py
+++ TriggerAnalyzer/newCRAB_forMC.py
@@ -3,6 +3,7 @@ config = Configuration()
 config.section_("General") 
 config.General.requestName = 'triggerTree_MC_80X_tthbb'
 #config.General.requestName = 'triggerTree_MC_80X_tthnonbb'
+#config.General.requestName = 'triggerTree_MC_80X_ttjets'
 #config.General.requestName = 'triggerTree_MC_80X_ttjets_semilep'
 #config.General.requestName = 'triggerTree_MC_80X_ttjets_dilep'
 #config.General.requestName = 'triggerTree_MC_80X_drell_yan'
@@ -17,6 +18,7 @@ config.JobType.maxJobRuntimeMin = 3600
 config.section_("Data") 
 config.Data.inputDataset = '/ttHTobb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/RunIISpring16MiniAODv2-premix_withHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM'
 #config.Data.inputDataset = '/ttHToNonbb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/RunIISpring16MiniAODv2-premix_withHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM'
+#config.Data.inputDataset = '/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext3-v1/MINIAODSIM'
 #config.Data.inputDataset = '/TTToSemilepton_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/RunIISpring16MiniAODv2-premix_withHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM'
 #config.Data.inputDataset = '/TTTo2L2Nu_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/RunIISpring16MiniAODv2-premix_withHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM'
 #config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM'
@@ -30,6 +32,7 @@ config.Data.publication = True
 config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter/' 
 config.Data.outputDatasetTag = 'triggerTree_MC_80X_tthbb' 
 #config.Data.outputDatasetTag = 'triggerTree_MC_80X_tthnonbb' 
+#config.Data.outputDatasetTag = 'triggerTree_MC_80X_ttjets'
 #config.Data.outputDatasetTag = 'triggerTree_MC_80X_ttjets_semilep'
 #config.Data.outputDatasetTag = 'triggerTree_MC_80X_ttjets_dilep'
 #config.Data.outputDatasetTag = 'triggerTree_MC_80X_drell_yan'
